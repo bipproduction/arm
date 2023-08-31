b795a2bb2053a780b739a5be131a0d7f1c4b1909
lukman <lukman@lukmans-MacBook-Air.local>
2023-08-31 17:50:25 +0800

    style: menambahkan tampilan dashboard
    Deskripsi:
    - nemambahkan tampilan header
    - mengubah tampilan navbar mobile
    - mengubah tampilan template
    - menambahkan function di bagian index
    
    No issue

--- src/modules/dashboard/component/navbar_mobile.tsx
+++ src/modules/dashboard/component/navbar_mobile.tsx
@@ -1,60 +1,108 @@
-'use client'
+"use client";
 
-import { isMobile } from "@/modules/_global";
-import { ActionIcon, Group, createStyles, rem } from "@mantine/core"
+import { COLOR, isMobile } from "@/modules/_global";
+import {
+  ActionIcon,
+  Box,
+  Center,
+  Group,
+  ScrollArea,
+  Text,
+  createStyles,
+  rem,
+} from "@mantine/core";
 import { useAtom } from "jotai";
-import { AiOutlineHome } from "react-icons/ai"
+import { LuHelpCircle, LuLayoutDashboard } from "react-icons/lu";
+import { HiOutlineBriefcase, HiOutlineCurrencyDollar, HiOutlineIdentification, HiOutlinePresentationChartLine } from "react-icons/hi";
 
 const useStyles = createStyles((theme) => ({
-    footer: {
-        marginTop: rem(120),
-        borderTop: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
-            }`,
-        position:'sticky',
-        bottom:0
+  footer: {
+    marginTop: rem(120),
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[1] : theme.colors.gray[1]
+    }`,
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[6],
+    position: "sticky",
+    bottom: 0,
+  },
 
+  inner: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: `${theme.spacing.mdoaszaz} ${theme.spacing.md}`,
+
+    [theme.fn.smallerThan("sm")]: {
+      flexDirection: "column",
     },
+  },
 
-    inner: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: `${theme.spacing.md} ${theme.spacing.md}`,
-
-        [theme.fn.smallerThan('sm')]: {
-            flexDirection: 'column',
-        },
+  links: {
+    [theme.fn.smallerThan("sm")]: {
+      marginTop: theme.spacing.lg,
+      marginBottom: theme.spacing.sm,
     },
-
-    links: {
-        [theme.fn.smallerThan('sm')]: {
-            marginTop: theme.spacing.lg,
-            marginBottom: theme.spacing.sm,
-        },
-    },
+  },
 }));
+const data = [
+  { key: "md1", link: "/dashboard", label: "Dashboard", icon: LuLayoutDashboard },
+  {
+    key: "md2",
+    link: "/dashboard/activities",
+    label: "Activities",
+    icon: HiOutlineIdentification,
+  },
+  {
+    key: "md3",
+    link: "/dashboard/assets",
+    label: "Assets",
+    icon: HiOutlineBriefcase,
+  },
+  {
+    key: "md4",
+    link: "/dashboard/bills",
+    label: "Bills",
+    icon: HiOutlineCurrencyDollar,
+  },
+  {
+    key: "md5",
+    link: "/dashboard/insights",
+    label: "Insights",
+    icon: HiOutlinePresentationChartLine,
+  },
+  {
+    key: "md6",
+    link: "/dashboard/help",
+    label: "Help",
+    icon: LuHelpCircle,
+  },
+];
 
 export function NavbarMobile() {
-    const { classes } = useStyles();
-    const [valMobile, setMobile] = useAtom(isMobile)
-    if(!valMobile) return <></>
-    return (
-        <>
-            <div className={classes.footer}>
-                <div className={classes.inner}>
-                    <Group spacing="xs" position="center" noWrap>
-                        <ActionIcon size="lg" variant="default" radius="xl">
-                            <AiOutlineHome size="1.05rem" stroke={1.5} />
-                        </ActionIcon>
-                        <ActionIcon size="lg" variant="default" radius="xl">
-                            <AiOutlineHome size="1.05rem" stroke={1.5} />
-                        </ActionIcon>
-                        <ActionIcon size="lg" variant="default" radius="xl">
-                            <AiOutlineHome size="1.05rem" stroke={1.5} />
-                        </ActionIcon>
-                    </Group>
-                </div>
-            </div>
-        </>
-    )
+  const { classes } = useStyles();
+  const [valMobile, setMobile] = useAtom(isMobile);
+  if (!valMobile) return <></>;
+  return (
+    <>
+      <div className={classes.footer}>
+        <div className={classes.inner}>
+          <Group spacing="xs"  noWrap>
+            {data.map((item) => {
+                return (
+            <Box style={{cursor: "pointer", textDecoration: "none"}} key={item.key} component="a"
+            href={item.link} m={9}>
+              <Center>
+              <ActionIcon color={"gray.0"}>
+                  <item.icon size="20"/>
+              </ActionIcon>
+              </Center>
+              <Text ta={"center"} color={"white"} fz={8}>{item.label}</Text>
+            </Box>
+                )
+            })}
+          </Group>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/modules/dashboard/view/dashboard_view.tsx
+++ src/modules/dashboard/view/dashboard_view.tsx
@@ -1,21 +1,43 @@
-'use client'
-import { useAtom } from 'jotai';
-import React, { useState } from 'react';
-import { isMobile } from '../../_global/val/isMobile';
-import { Text } from '@mantine/core';
-import { useShallowEffect } from '@mantine/hooks';
+"use client";
+import { useAtom } from "jotai";
+import React, { useState } from "react";
+import { isMobile } from "../../_global/val/isMobile";
+import { Box, Button, Group, SimpleGrid, Stack, Text } from "@mantine/core";
+import { useShallowEffect } from "@mantine/hooks";
+import { EchartsDashboard } from "..";
+import { COLOR } from "@/modules/_global";
 
 export function DashboardView() {
   const [inMobile, setMobile] = useAtom(isMobile);
 
-
-
-
   return (
     <>
-      {JSON.stringify(inMobile)}
+      {/* {JSON.stringify(inMobile)}
       <Text>Dashboard MObile</Text>
-      {inMobile && <Text>Mobile nihh</Text>}
+      {inMobile && <Text>Mobile nihh</Text>} */}
+      <Stack>
+        <Box>
+          <Group position="right">
+            <Button color="gray">CREATE PROJECTS</Button>
+            <Button color="gray">RECENT ACTIVITY</Button>
+          </Group>
+          <Box pt={40}>
+            <Box
+              sx={{
+                backgroundColor: "#C6C5C5",
+                borderRadius: 10,
+              }}
+            >
+              <Box p={10}>
+                <Text fw={700} pl={20} pt={20}>
+                  MONTHLY BUDGET STATISTICS
+                </Text>
+              </Box>
+              <EchartsDashboard />
+            </Box>
+          </Box>
+        </Box>
+      </Stack>
     </>
   );
 }
