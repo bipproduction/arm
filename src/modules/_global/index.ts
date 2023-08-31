f5c795a4a3d5d8f1c165eeba7e9e86a61b43a6fb
lukman <lukman@lukmans-MacBook-Air.local>
2023-08-31 17:55:38 +0800

    feat: code warna
    Deskripsi:
    - menambahkan Code Color
    - menambahkan function index
    
    No Issue

--- src/modules/_global/index.ts
+++ src/modules/_global/index.ts
@@ -1,3 +1,5 @@
 export { Pengaman } from './lib/pengaman'
 
 export { isMobile } from './val/isMobile'
+
+export {COLOR} from './val/COLOR'
---
+++ src/modules/_global/val/COLOR.ts
@@ -0,0 +1,6 @@
+export const COLOR = {
+  Hitam: "#040D12",
+  AbuAbu: "#61677A",
+  Putih: "#FFFFFF",
+  AbuMuda: "#9BA4B5"
+}
