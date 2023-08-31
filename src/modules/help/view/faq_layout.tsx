f7480b83038d1e1866a650e51c8144b5b1617c84
lukman <lukman@lukmans-MacBook-Air.local>
2023-08-31 17:47:24 +0800

    style: menambahkan tampilan help
    Deskripsi:
    - memperbaiki tampilan contact layout
    - memperbaiki tampilan faq layout
    - memperbaiki help view
    
    No issue

--- src/modules/help/view/faq_layout.tsx
+++ src/modules/help/view/faq_layout.tsx
@@ -1,60 +1,62 @@
-'use client'
+"use client";
 
-import { Accordion, Grid } from "@mantine/core"
+import { Accordion, Box, Grid, SimpleGrid } from "@mantine/core";
+
+const DataHelp = [
+  {
+    id: 1,
+    title: "How can I reset my password?",
+    isi: "It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.",
+  },
+  {
+    id: 2,
+    title: "How can I reset my password?",
+    isi: "It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.",
+  },
+  {
+    id: 3,
+    title: "How can I reset my password?",
+    isi: "It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.",
+  },
+  {
+    id: 4,
+    title: "How can I reset my password?",
+    isi: "It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.",
+  },
+];
 
 export function FaqLayout() {
-    return (
-        <>
-            <Grid>
-                <Grid.Col span={6}>
-                    <Accordion variant="contained">
-                        <Accordion.Item value="photos">
-                            <Accordion.Control>
-                                How can I reset my password?
-                            </Accordion.Control>
-                            <Accordion.Panel>It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.</Accordion.Panel>
-                        </Accordion.Item>
-
-                        <Accordion.Item value="print">
-                            <Accordion.Control>
-                                Can I create more that one account?
-                            </Accordion.Control>
-                            <Accordion.Panel>It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.</Accordion.Panel>
-                        </Accordion.Item>
-
-                        <Accordion.Item value="camera">
-                            <Accordion.Control>
-                                How can I subscribe to monthly newsletter?
-                            </Accordion.Control>
-                            <Accordion.Panel>It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.</Accordion.Panel>
-                        </Accordion.Item>
-                    </Accordion>
-                </Grid.Col>
-                <Grid.Col span={6}>
-                    <Accordion variant="contained">
-                        <Accordion.Item value="photos">
-                            <Accordion.Control>
-                                How can I reset my password?
-                            </Accordion.Control>
-                            <Accordion.Panel>It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.</Accordion.Panel>
-                        </Accordion.Item>
-
-                        <Accordion.Item value="print">
-                            <Accordion.Control>
-                                Can I create more that one account?
-                            </Accordion.Control>
-                            <Accordion.Panel>It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.</Accordion.Panel>
-                        </Accordion.Item>
-
-                        <Accordion.Item value="camera">
-                            <Accordion.Control>
-                                How can I subscribe to monthly newsletter?
-                            </Accordion.Control>
-                            <Accordion.Panel>It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.</Accordion.Panel>
-                        </Accordion.Item>
-                    </Accordion>
-                </Grid.Col>
-            </Grid>
-        </>
-    )
+  return (
+    <>
+      <SimpleGrid
+        cols={2}
+        spacing="lg"
+        breakpoints={[
+          { maxWidth: "sm", cols: 2, spacing: "sm" },
+          { maxWidth: "xs", cols: 1, spacing: "sm" },
+        ]}
+      >
+        <Box>
+          {DataHelp.map((item) => (
+            <Accordion variant="contained" key={item.id}>
+              <Accordion.Item value="photos">
+                <Accordion.Control>{item.title}</Accordion.Control>
+                <Accordion.Panel>{item.isi}</Accordion.Panel>
+              </Accordion.Item>
+            </Accordion>
+          ))}
+        </Box>
+        <Box>
+          {DataHelp.map((item) => (
+            <Accordion variant="contained" key={item.id}>
+              <Accordion.Item value="photos">
+                <Accordion.Control>{item.title}</Accordion.Control>
+                <Accordion.Panel>{item.isi}</Accordion.Panel>
+              </Accordion.Item>
+            </Accordion>
+          ))}
+        </Box>
+      </SimpleGrid>
+    </>
+  );
 }
