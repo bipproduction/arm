29f07ecaf4461aa633d11984f7a36cf3cc935679
lukman <lukman@lukmans-MacBook-Air.local>
2023-09-06 17:54:08 +0800

    style: update dashboard
    Deskripsi:
    - update echart
    
    No Issue

--- src/modules/dashboard/view/dashboard_view.tsx
+++ src/modules/dashboard/view/dashboard_view.tsx
@@ -6,39 +6,27 @@ import { Box, Button, Group, SimpleGrid, Stack, Text } from "@mantine/core";
 import { useShallowEffect } from "@mantine/hooks";
 import { COLOR, PageTitle } from "@/modules/_global";
 import { EchartsDashboard } from "../component/echarts_dashboard";
+import { useRouter } from "next/navigation";
 
 export function DashboardView() {
   const [inMobile, setMobile] = useAtom(isMobile);
+  const router = useRouter()
 
   return (
     <>
-      {/* {JSON.stringify(inMobile)}
-      <Text>Dashboard MObile</Text>
-      {inMobile && <Text>Mobile nihh</Text>} */}
       <Stack>
         <Box pt={10}>
         <PageTitle text="Dashboard"/>
           <Group position="right">
-            <Button color="gray" component="a" href="/dashboard/list-client">CREATE PROJECTS</Button>
-            <Button color="gray">RECENT ACTIVITY</Button>
+            <Button color="gray" onClick={() => router.push("/dashboard/list-client")}>CREATE PROJECTS</Button>
+            <Button color="gray" onClick={() => router.push("/dashboard/recent-activity")}>RECENT ACTIVITY</Button>
           </Group>
-          <Box pt={40}>
-            <Box
-              sx={{
-                backgroundColor: "#C6C5C5",
-                borderRadius: 10,
-              }}
-            >
-              <Box p={10}>
-                <Text fw={700} pl={20} pt={20}>
-                  MONTHLY BUDGET STATISTICS
-                </Text>
-              </Box>
-              <EchartsDashboard />
-            </Box>
-          </Box>
+
         </Box>
       </Stack>
+        <Box>
+          <EchartsDashboard/>
+        </Box>
     </>
   );
 }
