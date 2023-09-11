d67c6b5ce97d72dd0b6765d65d1cf992aeae688d
lukman <lukman@lukmans-MacBook-Air.local>
2023-09-11 16:34:32 +0800

    style: update archive
    Deskripsi:
    - merapikan coding project brief
    - merapikan coding timeline
    
    No Issue

--- src/modules/archive/view/modal_archive.tsx
+++ src/modules/archive/view/modal_archive.tsx
@@ -1,28 +1,27 @@
-import { Alert, Box, Button, Grid, Text } from '@mantine/core';
-import { useAtom } from 'jotai';
-import { atomWithStorage } from 'jotai/utils';
-import { useRouter } from 'next/navigation';
-import React from 'react';
-import toast from 'react-simple-toasts';
-import "react-simple-toasts/dist/theme/dark.css"
-import { isArchiveTamplate } from '../val/idModalArchive';
-const val_archive = atomWithStorage("val_archive", false)
-
+import { Alert, Box, Button, Grid, Text } from "@mantine/core";
+import { useAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
+import { useRouter } from "next/navigation";
+import React from "react";
+import toast from "react-simple-toasts";
+import "react-simple-toasts/dist/theme/dark.css";
+import { isArchiveTamplate } from "../val/idModalArchive";
+const val_archive = atomWithStorage("val_archive", false);
 
 export default function ModalArchiveView() {
-  const [openArchive, setOpenArchive] = useAtom(isArchiveTamplate)
-  const router = useRouter()
+  const [openArchive, setOpenArchive] = useAtom(isArchiveTamplate);
+  const router = useRouter();
   function archiveCreate() {
-    toast("Success", {theme: "dark"})
-    router.push('/dashboard/project/create')
-    setOpenArchive(false)
+    toast("Success", { theme: "dark" });
+    router.push("/dashboard/project/create");
+    setOpenArchive(false);
   }
   return (
-   <>
-         <Box>
+    <>
+      <Box>
         <Alert color="gray" variant="outline">
           <Text fw={700} ta={"center"} mb={20} mt={20}>
-          ARE YOU SURE USING THIS TEMPLATE?
+            ARE YOU SURE USING THIS TEMPLATE?
           </Text>
           <Grid>
             <Grid.Col span={6}>
@@ -39,8 +38,8 @@ export default function ModalArchiveView() {
               <Button
                 radius={10}
                 color="gray.7"
-               fullWidth
-               onClick={archiveCreate}
+                fullWidth
+                onClick={archiveCreate}
               >
                 YES
               </Button>
@@ -48,7 +47,6 @@ export default function ModalArchiveView() {
           </Grid>
         </Alert>
       </Box>
-   </>
+    </>
   );
 }
-
