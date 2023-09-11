dedb1f57d0bf4d2035901d27a5b4753109d38f31
lukman <lukman@lukmans-MacBook-Air.local>
2023-09-11 16:33:31 +0800

    style: update activities
    Deskripsi:
    - merapikan coding archive
    - merapikan coding create project
    - merapikan coding delovery status
    - merapikan coding quotation
    - update modal create project
    
    No issue

--- src/modules/activities/components/quotation.tsx
+++ src/modules/activities/components/quotation.tsx
@@ -1,13 +1,17 @@
-"use client"
+"use client";
 import { Button, Text } from "@mantine/core";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 export function Quotation() {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <>
-      <Button fullWidth color="gray.0" onClick={() => router.push(`/dashboard/quotation`)}>
+      <Button
+        fullWidth
+        color="gray.0"
+        onClick={() => router.push(`/dashboard/quotation`)}
+      >
         <Text color="dark">QUOTATION</Text>
       </Button>
     </>
