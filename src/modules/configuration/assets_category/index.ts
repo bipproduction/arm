2bb2cb16306dfafd045e8b743bbadd2f647abde7
lukman <lukman@lukmans-MacBook-Air.local>
2023-09-14 17:36:00 +0800

    style: update assets category
    Deskripsi:
    - update edit
    - update index
    
    No Issue

--- src/modules/configuration/assets_category/index.ts
+++ src/modules/configuration/assets_category/index.ts
@@ -1,7 +1,9 @@
 import CreateAssetsCategory from "./view/create_assets_category";
 import EditAssetsCategory from "./view/edit_assets_category";
 import TableAssetsCategory from "./view/table_assets_category";
+import { funGetAllAssetsCategory } from "./fun/assets_category_get_all";
 
-export {CreateAssetsCategory}
-export {EditAssetsCategory}
-export {TableAssetsCategory}
+export { CreateAssetsCategory };
+export { EditAssetsCategory };
+export { TableAssetsCategory };
+export { funGetAllAssetsCategory };
