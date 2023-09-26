681025285c9ec1fb9c54d5f7d0a878a8bd7b51cf
amel <amel@amel.local>
2023-09-26 17:29:51 +0800

    feature : profile user
    Deskripsi :
    - backend get profile
    - backend update profile
    - API images
    
    No Issues

--- src/modules/profile/index.ts
+++ src/modules/profile/index.ts
@@ -1,5 +1,7 @@
 import EditView from "./view/edit";
 import ProfileView from "./view/profile";
+import { funGetProfile } from "./fun/get_profile";
 
-export {EditView}
-export {ProfileView}
+export { EditView }
+export { ProfileView }
+export { funGetProfile }
